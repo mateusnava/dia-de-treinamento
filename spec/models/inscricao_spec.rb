d2ae5ad5f64c3f2c2b14b0be005fcaed7c5f685f
Mateus Nava <mateus@inocode.com.br>
2015-07-29 20:47:42 -0300

    Teste para scope participantes

--- spec/models/inscricao_spec.rb
+++ spec/models/inscricao_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe Inscricao, :type => :model do
+RSpec.describe Inscricao, type: :model do
   describe '.participantes' do
-    # TODO: RODRIGAR
+    context 'deve retornar inscritos que participaram do evento' do
+      let(:participante) { create(:pessoa) }
+      let(:nao_participante) { create(:pessoa) }
+      let!(:inscricao_1) { create(:inscricao, pessoa: participante, participou: true) }
+      let!(:inscricao_2) { create(:inscricao, pessoa: nao_participante, participou: false) }
+
+      it { expect(Inscricao.participantes.pluck(:pessoa_id)).to eq([participante.id]) }
+    end
   end
 end

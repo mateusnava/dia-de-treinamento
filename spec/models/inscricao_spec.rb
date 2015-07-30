7a52475ddb70f1046945a802f91fef790e0eeeba
Mateus Nava <mateus@inocode.com.br>
2015-07-29 21:33:41 -0300

    Issue #2 Inicializar a coluna PARTICIPOU da Inscricao com false

--- spec/models/inscricao_spec.rb
+++ spec/models/inscricao_spec.rb
@@ -5,10 +5,21 @@ RSpec.describe Inscricao, type: :model do
     context 'deve retornar inscritos que participaram do evento' do
       let(:participante) { create(:pessoa) }
       let(:nao_participante) { create(:pessoa) }
-      let!(:inscricao_1) { create(:inscricao, pessoa: participante, participou: true) }
-      let!(:inscricao_2) { create(:inscricao, pessoa: nao_participante, participou: false) }
+      let!(:inscricao_1) { create(:inscricao_participante, pessoa: participante) }
+      let!(:inscricao_2) { create(:inscricao_nao_participante, pessoa: nao_participante) }
 
       it { expect(Inscricao.participantes.pluck(:pessoa_id)).to eq([participante.id]) }
+    end
+  end
+
+  describe 'deve setar participou como false por padrão' do
+    context 'quando nil' do
+      it { expect(create(:inscricao, participou: nil).participou).to_not be }
+    end
+
+    context 'quando não nil' do
+      it { expect(create(:inscricao_participante).participou).to be }
+      it { expect(create(:inscricao_nao_participante).participou).to_not be }
     end
   end
 end

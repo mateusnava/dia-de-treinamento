c4aa01714dc337690bb73af927c8d99d83f8a3df
Mateus Nava <mateus@inocode.com.br>
2015-10-17 13:23:26 -0300

    Criação de helper para exibição de mensagens 'flash'

--- app/assets/javascripts/geral.js
+++ app/assets/javascripts/geral.js
@@ -4,8 +4,4 @@ $(document).on('page:change', function(e) {
   $('[data-form-submit]').on('change', function() {
     $(this).parents('form:first').submit();
   });
-
-  setTimeout(function() {
-    $('#messages').hide('slow');
-  }, 1500);
 });

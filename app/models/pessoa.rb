2019edc6d65061f0941e4bc82f23889899375dca
Mateus Nava <mateus@inocode.com.br>
2015-10-22 10:44:17 -0200

    Issue #21 - Validação de dados na inscrição

--- app/models/pessoa.rb
+++ app/models/pessoa.rb
@@ -1,10 +1,8 @@
 class Pessoa < ActiveRecord::Base
+  EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 
   validates :nome, :email, presence: true
   validates :email, uniqueness: true
-
-  def inscrever
-    
-  end
-
+  validates :nome, format: { with: /([a-zA-Z]*\s)+/, message: I18n.t('errors.messages.full_name') }
+  validates :email, format: { with: EMAIL_REGEXP, message: I18n.t('errors.messages.email') }
 end
--- spec/models/pessoa_spec.rb
+++ spec/models/pessoa_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Pessoa, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validar nome completo' do
+    context 'quando não for nome completo' do
+      it 'deve ocorrer erro' do
+        expect(build(:pessoa, nome: 'NomeNaoCompleto')).to_not be_valid
+        expect(build(:pessoa, nome: '')).to_not be_valid
+        expect(build(:pessoa, nome: nil)).to_not be_valid
+      end
+    end
+
+    context 'quando for nome completo' do
+      it 'não deve ocorrer erro' do
+        expect(build(:pessoa, nome: 'Nome Completo')).to be_valid
+        expect(build(:pessoa, nome: 'Nome Super Completo')).to be_valid
+        expect(build(:pessoa, nome: 'Martin Luther King, Jr.')).to be_valid
+        expect(build(:pessoa, nome: 'Mathias d\'Arras')).to be_valid
+        expect(build(:pessoa, nome: 'Martin king, Jr.-')).to be_valid
+      end
+    end
+  end
+
+  describe 'validar e-mail' do
+    context 'quando não for e-mail válido' do
+      it 'deve ocorrer erro' do
+        expect(build(:pessoa, email: 'teste@')).to_not be_valid
+        expect(build(:pessoa, email: 'teste@asd')).to_not be_valid
+        expect(build(:pessoa, email: '@asd.com')).to_not be_valid
+        expect(build(:pessoa, email: '')).to_not be_valid
+        expect(build(:pessoa, email: nil)).to_not be_valid
+      end
+    end
+
+    context 'quando for nome completo' do
+      it 'não deve ocorrer erro' do
+        expect(build(:pessoa, email: 'teste@teste.com')).to be_valid
+        expect(build(:pessoa, email: 'teste@teste.com.br')).to be_valid
+        expect(build(:pessoa, email: 'teste.teste@teste.com')).to be_valid
+      end
+    end
+  end
 end

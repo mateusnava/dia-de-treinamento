f9b3db4ad410166c4cd014e22ac41b1a6f056c70
Mateus Nava <mateus@inocode.com.br>
2015-10-22 10:45:40 -0200

    Regex de nome completo em constante

--- app/models/pessoa.rb
+++ app/models/pessoa.rb
@@ -1,8 +1,9 @@
 class Pessoa < ActiveRecord::Base
   EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+  NOME_COMPLETO_REGEX = /([a-zA-Z]*\s)+/
 
   validates :nome, :email, presence: true
   validates :email, uniqueness: true
-  validates :nome, format: { with: /([a-zA-Z]*\s)+/, message: I18n.t('errors.messages.full_name') }
+  validates :nome, format: { with: NOME_COMPLETO_REGEX, message: I18n.t('errors.messages.full_name') }
   validates :email, format: { with: EMAIL_REGEXP, message: I18n.t('errors.messages.email') }
 end

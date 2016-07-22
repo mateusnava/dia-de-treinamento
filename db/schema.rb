e2afd1abb839ee698ec1d00b0da0a4e8082deb35
Mateus Nava <mateus@inocode.com.br>
2016-07-21 21:01:47 -0300

    Adicionado logo da agro1

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,25 @@
 
 ActiveRecord::Schema.define(version: 20151026120910) do
 
+  create_table "administradores", force: true do |t|
+    t.string   "nome"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+  end
+
+  add_index "administradores", ["email"], name: "index_administradores_on_email", unique: true, using: :btree
+  add_index "administradores", ["reset_password_token"], name: "index_administradores_on_reset_password_token", unique: true, using: :btree
+
   create_table "eventos", force: true do |t|
     t.string   "nome"
     t.integer  "vagas"
@@ -50,7 +69,24 @@ ActiveRecord::Schema.define(version: 20151026120910) do
     t.string   "instituicao"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
+
+  add_index "pessoas", ["confirmation_token"], name: "index_pessoas_on_confirmation_token", unique: true, using: :btree
+  add_index "pessoas", ["email"], name: "index_pessoas_on_email", unique: true, using: :btree
+  add_index "pessoas", ["reset_password_token"], name: "index_pessoas_on_reset_password_token", unique: true, using: :btree
 
   create_table "usuarios", force: true do |t|
     t.string   "email",                  default: "", null: false
